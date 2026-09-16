187728bc6e0b1b061edd59de5b8b0dce73571ce2
hima97u <work.himan7u@gmail.com>
2026-09-16 11:10:42 +0530

    feat(api): update URL configuration

--- ecommerceApiProject/urls.py
+++ ecommerceApiProject/urls.py
@@ -18,9 +18,15 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
+from apiApp.views import health_check
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/auth/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('api/health/', health_check, name='health_check'),
     path('',include("apiApp.urls"))
 ]
 
